a34e6fa063dfb6945e45d98c36be0cad316ad127
Shawn-Fries <samfoley275@gmail.com>
2021-12-13 16:12:28 -0500

    First problem partway

--- w3d1.rb
+++ w3d1.rb
@@ -1,3 +1,11 @@
-def test
-    #changes
+class Array
+    def my_each(&block)
+        i = 0
+        result = []
+        while i < self.length
+            result << block.call(self[i])
+            i += 1
+        end
+        result
+    end
 end
