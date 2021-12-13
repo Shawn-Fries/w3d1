167a9165b0ce83ef5735ea0a1f8fd19189a360f8
kirbyneaton <kjneat@gmail.com>
2021-12-13 16:41:44 -0500

    my_select update

--- w3d1.rb
+++ w3d1.rb
@@ -1,11 +1,34 @@
 class Array
     def my_each(&block)
         i = 0
-        result = []
+        
         while i < self.length
-            result << block.call(self[i])
+            block.call(self[i])
             i += 1
+        end
+        self
+    end
+
+    def my_select(&block)
+        result = []
+        self.my_each do |ele|
+            result << ele if block.call(ele)
         end
         result
     end
+
+    # def my_select(&block)
+    #     i = 0
+    #     result = []
+    #     while i < self.length
+    #         result << self[i] if block.call(self[i])
+    #         i += 1
+    #     end
+    #     result 
+    # end
+
 end
+
+
+
+
