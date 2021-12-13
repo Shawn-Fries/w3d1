37a7b1fa50b2f48da5591da04f4713be86aa894d
Shawn-Fries <samfoley275@gmail.com>
2021-12-13 17:19:16 -0500

    on my_flatten

--- w3d1.rb
+++ w3d1.rb
@@ -17,16 +17,42 @@ class Array
         result
     end
 
-    # def my_select(&block)
-    #     i = 0
-    #     result = []
-    #     while i < self.length
-    #         result << self[i] if block.call(self[i])
-    #         i += 1
-    #     end
-    #     result 
-    # end
+   def my_reject(&block)
+       result = []
+       self.my_each do |ele|
+            result << ele if !block.call(ele)
+        end
 
+       result
+    end
+
+    def my_any?(&block)
+        self.my_each do |ele|
+            return true if block.call(ele)
+        end
+        false
+    end
+
+    def my_all?(&block)
+        self.my_each do |ele|
+            return false if !block.call(ele)
+        end
+        true
+    end
+
+    def my_flatten(array)
+        result = []
+        if array.is_a?(Array)
+
+            array.each do |array|
+                my_flatten(array)
+            end
+        else
+            result << array
+        end
+
+        result
+    end
 end
 
 
